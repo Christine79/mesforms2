20fb802adb23b5c55945344f0c8c6411641317b4
christine <christinejaouen@hotmail.com>
2018-10-30 22:49:29 +0100

    commit2

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'user/new'
   get 'user/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/mesforms/new' , to: 'user#new'
-  get '/mesforms/create' , to: 'user#create'
+  get '/mesforms2/new' , to: 'user#new'
+  get '/mesforms2/create' , to: 'user#create'
 end
